94b54f45c8e668450ad6bb48aacc33aa216f2af2
Greg Funtera <greg.funtera@gmail.com>
2018-05-11 22:50:59 +0800

    - Added Rendering of Blog List - Added Rendering of Blog View

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -1,41 +1,106 @@
 var moment = require('moment');
+var request = require('request');
+
+var apiOptions = {
+  server: 'http://localhost:3000/'
+};
+
+if(process.env.NODE_ENV === 'production') {
+  apiOptions.server = 'https://potatocoder.herokuapp.com/';
+}
 
 module.exports.blogList = function(req, res) {
+  var requestOptions, path;
+  path = 'api/blog';
+  
+  requestOptions = {
+    url: apiOptions.server + path,
+    method: 'GET',
+    json: {},
+  };
+
+  request(requestOptions, function(err, response, body) {
+    renderBlogList(req, res, body);
+  });
+
+}
+
+var renderBlogList = function(req, res, responseBody) {
+  var message;
+  
+  message = createBlogListMessage(responseBody).message;
+  responseBody = createBlogListMessage(responseBody).response;
+
   res.render('blog-list', {
-    topics: [
-      {
-        title: 'Sample Title 1',
-        summary: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more    recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-        featured_photo: 'http://www.lirent.net/wp-content/uploads/2014/10/Android-Lollipop-wallpapers-p-800x500.png',
-      },
-      {
-        title: 'Sample Title 2',
-        summary: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more    recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-        featured_photo: 'http://www.lirent.net/wp-content/uploads/2014/10/Android-Lollipop-wallpapers-p-800x500.png',
-      },
-      {
-        title: 'Sample Title 3',
-        summary: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more    recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-        featured_photo: 'http://www.lirent.net/wp-content/uploads/2014/10/Android-Lollipop-wallpapers-p-800x500.png',
-      },
-    ]
+    topics: responseBody,
+    message: message,
+  });
+};
+
+var createBlogListMessage = function(responseBody) {
+  var message, response = responseBody;
+  if(!Array.isArray(responseBody)) {
+    message = 'API lookup error';
+    response = [];
+  } else {
+    if(!responseBody.length) {
+      message = 'No topics to be found'
+    } 
+  }
+  return {message: message, response: response};
+}
+
+
+
+
+module.exports.blogView = function(req, res) {
+  getBlogView(req, res, function(req, res, data) {
+    renderBlogView(req, res, data);
+  })
+}
+
+var getBlogView = function(req, res, callback) {
+  var blogid = req.params.blogid;
+  
+  var requestOptions, path = 'api/blog/';
+  requestOptions = {
+    url: apiOptions.server + path + blogid,
+    method: 'GET',
+    json: {},
+  }
+  
+  request(requestOptions, function(error, response, body) {
+    if(response.statusCode === 200) {
+      callback(req, res, body);
+    } else {
+      _showError(req, res, response.statusCode);
+    }
   });
 }
 
-module.exports.blogView = function(req, res) {
-  let timestamp = new Date().getTime();
+var renderBlogView = function(req, res, responseBody) {
   res.render('blog-view', {
-    title: 'Sample Title 3',
-    body: `
-          <h3>What is Lorem Ipsum?</h3>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-          <h3>Why do we use it?</h3>
-          <p>
-            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-          </p>`,
-    featured_photo: 'http://www.lirent.net/wp-content/uploads/2014/10/Android-Lollipop-wallpapers-p-800x500.png',
-    created_at: moment(timestamp).format("MMM Do YY"),
+    title: responseBody.title,
+    body: responseBody.body,
+    featured_photo: responseBody.featured_photo,
+    created_at: moment(responseBody.created_at).format("MMM Do YY"),
+  });
+}
+
+var _showError = function(req, res, status) {
+   var title, content; 
+  if(status === 404) {
+    title = status + ', page not found';
+    content = `Oh dear, looks like we can't find this page.`;
+  } else {
+    title = status + ', something gone wrong';
+    content = `Something somewhere, has gone a little bit wrong.`
+  }
+
+  res.status(status);
+
+  res.render('blog-view-error', {
+    title: title,
+    content: content
   });
 }
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -4,6 +4,6 @@ var blogs = require('../controllers/blog')
 
 /* GET home page. */
 router.get('/', blogs.blogList);
-router.get('/blog/blogid', blogs.blogView)
+router.get('/blog/:blogid', blogs.blogView)
 
 module.exports = router;
