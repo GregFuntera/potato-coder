169925d0984b3a384a8a8808c18f1c465d321493
Greg Funtera <greg.funtera@gmail.com>
2018-05-13 13:06:31 +0800

    - Added cors

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,16 +1,20 @@
 var express = require('express');
+var cors = require('cors');
 var router = express.Router();
 var blogCtrl = require('../controllers/blog');
+var app = express();
+
+app.use(cors());
 
 // Get list of Blogs
-router.get('/blog', blogCtrl.getBlogs);
+router.get('/blog', cors(), blogCtrl.getBlogs);
 // Get a specific Blog
-router.get('/blog/:blogid', blogCtrl.getBlog);
+router.get('/blog/:blogid', cors(), blogCtrl.getBlog);
 // Post a new Blog
-router.post('/blog', blogCtrl.createBlog);
+router.post('/blog', cors(), blogCtrl.createBlog);
 // Update a blog
-router.put('/blog/:blogid', blogCtrl.updateBlog);
+router.put('/blog/:blogid', cors(), blogCtrl.updateBlog);
 // Delete a Blog
-router.delete('/blog/:blogid', blogCtrl.deleteBlog);
+router.delete('/blog/:blogid', cors(), blogCtrl.deleteBlog);
 
 module.exports = router;
