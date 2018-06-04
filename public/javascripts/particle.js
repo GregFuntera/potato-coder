78933fab2e95d3f54bd5f9e3d969eaa2d48698d7
Greg Funtera <greg.funtera@gmail.com>
2018-06-04 17:30:12 +0800

    - Finished enhancements in blog home

--- public/javascripts/particle.js
+++ public/javascripts/particle.js
@@ -2,16 +2,16 @@ window.onload = function() {
     Particles.init({
         selector: '.background-particle',
         color: ['#007bff', '#00C9B1'],
-        connectParticles: true,
+        connectParticles: false,
         sizeVariations: 5,
-        maxParticles: 250,
+        maxParticles: 500,
         speed: 2,
         responsive: [
             {
                 breakpoint: 850,
                 options: {
                   color: '#00C9B1',
-                  maxParticles: 80,
+                  maxParticles: 500,
                 connectParticles: false
               }
             }
